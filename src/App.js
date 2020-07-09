efb5fef509a399160385bd364850abefefa60f18
Thomas Doneux <thomas.doneux@actito.com>
2020-07-09 10:12:28 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useReducer, useCallback } from 'react';
+import useSemiPersistentState from './components/hooks/useSemiPersistentState';
+import storiesReducer from './components/reducers/storiesReducer';
+import SearchForm from './components/ui/SearchForm';
+import List from './components/ui/List';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
+const App = () => {
+	const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+	const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
+
+	const [stories, dispatchStories] = useReducer(storiesReducer, {
+		data: [],
+		isLoading: false,
+		isError: false,
+	});
+
+	const handleFetchStories = useCallback(async () => {
+		dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+		try {
+			const result = await axios.get(url);
+
+			dispatchStories({
+				type: 'STORIES_FETCH_SUCCESS',
+				payload: result.data.hits,
+			});
+		} catch {
+			dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
+		}
+	}, [url]);
+
+	useEffect(() => {
+		handleFetchStories();
+	}, [handleFetchStories]);
+
+	const handleRemoveStory = (item) => {
+		dispatchStories({
+			type: 'REMOVE_STORY',
+			payload: item,
+		});
+	};
+
+	const handleSearchInput = (event) => {
+		setSearchTerm(event.target.value);
+	};
+
+	const handleSearchSubmit = (event) => {
+		setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+		event.preventDefault();
+	};
+
+	return (
+		<div className='container'>
+			<h1 className='headline-primary'>My Hacker Stories</h1>
+
+			<SearchForm
+				searchTerm={searchTerm}
+				onSearchInput={handleSearchInput}
+				onSearchSubmit={handleSearchSubmit}
+			/>
+
+			{stories.isError && <p>Something went wrong ...</p>}
+
+			{stories.isLoading ? (
+				<p>Loading ...</p>
+			) : (
+				<List list={stories.data} onRemoveItem={handleRemoveStory} />
+			)}
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/ui/List.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Item from './Item';
+
+const List = ({ list, onRemoveItem }) =>
+	list.map((item) => (
+		<Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+	));
+
+export default List;
